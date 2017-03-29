ee26a7f1b0c9f699ac848e531e31ca934cd66abd
juliette engel <juliette.engel@skema.edu>
2017-03-29 19:10:44 +0200

    add level selection to training on active admin

--- app/models/training.rb
+++ app/models/training.rb
@@ -10,8 +10,8 @@ class Training < ApplicationRecord
   #validates :location, :public_description, :private_description, presence: true, on: :update
   #validates :public_description, :private_description, length: { minimum: 140 }, on: :update
   validate :date_cannot_be_in_the_past, on: :create
-  validates :level, presence: true
-  validates_inclusion_of :level, :in => LEVELS
+  # validates :level, presence: true
+  # validates_inclusion_of :level, :in => LEVELS
   # validate :date_has_to_be_in_session
 
   scope :session_is, -> (session) { where(session: session) }
