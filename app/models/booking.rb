c820a7360f5d2661755f128e513674d2fe43324d
juliette engel <juliette.engel@skema.edu>
2017-04-06 14:49:04 +0200

    bug fixed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,7 @@ class Booking < ApplicationRecord
 
 	validates :user_id, presence: true
 	validates :training_id, presence: true
+  cattr_accessor :current_user
 
 	scope :past, -> { joins(:training).merge(Training.past.order(:date)) }
 	scope :upcoming, -> { joins(:training).merge(Training.upcoming.order(:date)) }
@@ -23,9 +24,9 @@ class Booking < ApplicationRecord
   end
 
   def notify_admin_cancellation # TODO
-    # unless current_user == training.city.coach
+    unless current_user == training.city.coach
     BookingMailer.cancelled(self).deliver_now
-    # end
+    end
   end
 
 end
