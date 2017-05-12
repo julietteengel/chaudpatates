f68f5115170edd7e0a482e430256817155878453
juliette engel <juliette.engel@skema.edu>
2017-05-12 17:51:50 +0200

    added method to give promocode to users already in db

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,10 +6,10 @@ def home
     # User.new.add_promocode_to_users_in_db
     @users = User.where(promocode: nil)
       @users.each do |u|
-      u.promocode = loop do
-      random_token = SecureRandom.urlsafe_base64(nil, false)
-      break random_token unless u.class.exists?(promocode: random_token)
-      end
+        u.promocode = loop do
+          random_token = SecureRandom.urlsafe_base64(nil, false)
+          break random_token unless u.class.exists?(promocode: random_token)
+        end
       u.save
       UserMailer.send_promocode(u)
     end
