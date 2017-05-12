ce139831785c7a9466cc890c653f081272590233
juliette engel <juliette.engel@skema.edu>
2017-05-12 17:48:25 +0200

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,11 @@ class PagesController < ApplicationController
 def home
     @cities = City.all
     # User.new.add_promocode_to_users_in_db
-    @user = User.where(promocode: nil)
-      @user.each do |u|
-      self.promocode = loop do
+    @users = User.where(promocode: nil)
+      @users.each do |u|
+      u.promocode = loop do
       random_token = SecureRandom.urlsafe_base64(nil, false)
-      break random_token unless self.class.exists?(promocode: random_token)
+      break random_token unless u.class.exists?(promocode: random_token)
       end
       u.save
       UserMailer.send_promocode(u)
