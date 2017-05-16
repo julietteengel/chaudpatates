f92e760d139d50124056514c2b9f6028a34cf0a3
juliette engel <juliette.engel@skema.edu>
2017-05-16 20:07:34 +0200

    erased part - sending email to users who didnt have promocode

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,17 +2,17 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
 def home
-    @cities = City.all
-    # User.new.add_promocode_to_users_in_db
-    @users = User.where(promocode: nil)
-      @users.each do |u|
-        u.promocode = loop do
-          random_token = SecureRandom.urlsafe_base64(nil, false)
-          break random_token unless u.class.exists?(promocode: random_token)
-        end
-      u.save
-      UserMailer.send_promocode(u).deliver_now
-    end
+    # @cities = City.all
+    # # User.new.add_promocode_to_users_in_db
+    # @users = User.where(promocode: nil)
+    #   @users.each do |u|
+    #     u.promocode = loop do
+    #       random_token = SecureRandom.urlsafe_base64(nil, false)
+    #       break random_token unless u.class.exists?(promocode: random_token)
+    #     end
+    #   u.save
+    #   UserMailer.send_promocode(u).deliver_now
+    # end
   end
 
   def show
