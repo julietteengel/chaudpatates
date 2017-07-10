c0ad654945a9d907f23ef5e9fcdcd7dbb5441c2e
juliette engel <juliette.engel@skema.edu>
2017-07-10 13:58:24 +1000

    ok

--- app/jobs/update_trainings_job.rb
+++ app/jobs/update_trainings_job.rb
@@ -1,28 +1,28 @@
-# class UpdateTrainingsJob < ApplicationJob
-#   queue_as :default
+class UpdateTrainingsJob < ApplicationJob
+  queue_as :default
 
-# 	DAYS_OF_WEEK = { Monday: 0, Tuesday: 1, Wednesday: 2, Thursday: 3, Friday: 4, Saturday: 5, Sunday: 6 }
+	DAYS_OF_WEEK = { Monday: 0, Tuesday: 1, Wednesday: 2, Thursday: 3, Friday: 4, Saturday: 5, Sunday: 6 }
 
-#   def perform(session, city)
-#   	trainings = Training.session_is(session).upcoming
-#   	time = session.time_of_day
-#   	new_day = session.day.to_sym
-#   	trainings.each do |training|
-#   		day = update_training_day(training, new_day)
-#   		set_training_date(training, time, day)
-#   		training.save!
-#   	end
-#   end
+  def perform(session, city)
+  	trainings = Training.session_is(session).upcoming
+  	time = session.time_of_day
+  	new_day = session.day.to_sym
+  	trainings.each do |training|
+  		day = update_training_day(training, new_day)
+  		set_training_date(training, time, day)
+  		training.save!
+  	end
+  end
 
-#   private
+  private
 
-#   def update_training_day(training, new_day)
-#   	training.date.next_week.advance(days: DAYS_OF_WEEK[new_day])
-#   end
+  def update_training_day(training, new_day)
+  	training.date.next_week.advance(days: DAYS_OF_WEEK[new_day])
+  end
 
-#   def set_training_date(training, time, day)
-# 		training.date = day.to_datetime + time.seconds_since_midnight.seconds
-#   end
+  def set_training_date(training, time, day)
+		training.date = day.to_datetime + time.seconds_since_midnight.seconds
+  end
 
-# end
+end
 
