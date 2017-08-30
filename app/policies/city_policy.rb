0589738c038bc97826481808dde4583419c88b64
juliette engel <juliette.engel@skema.edu>
2017-08-30 13:23:43 +1000

    photo showing on city page when member becomes user

--- app/policies/city_policy.rb
+++ app/policies/city_policy.rb
@@ -1,9 +1,10 @@
 class CityPolicy < ApplicationPolicy
 
 	def show?
+    # TODO : if public, only members and admin can see the page
 		true
 	end
-	
+
   class Scope < Scope
     def resolve
       scope
