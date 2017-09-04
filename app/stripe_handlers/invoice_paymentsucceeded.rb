a011c58feaf26c72972222fb3455639f5c2659cb
juliette engel <juliette.engel@skema.edu>
2017-09-04 12:49:49 +0200

    abonnement mensuel ne s'annule plus automatiquement apres trois mois

--- app/stripe_handlers/invoice_paymentsucceeded.rb
+++ app/stripe_handlers/invoice_paymentsucceeded.rb
@@ -6,7 +6,7 @@ class InvoicePaymentsucceeded
     all_invoices = Stripe::Invoice.all(:customer => stripe_id)
     customer = Stripe::Customer.retrieve(stripe_id)
     user = User.find_by_email(customer.email)
-    puts "invoice succeded ##################"
+    # puts "invoice succeded ##################"
     # paid_invoices = all_invoices.all(:paid => true)
     # if plan: 3-months and nb payments = 3 - cancel sub and send email
     # fonctionne si le client n'a souscrit qu'à un type d'abonnement depuis le début.
@@ -14,7 +14,8 @@ class InvoicePaymentsucceeded
     #   si suscribed à tel truc, user.suscribed en argument avec le plan
     #   là t's un true ou false
     if (all_invoices.count == 3) && (stripe_subscription.plan.id == "3_months_subscription")
-      stripe_subscription.delete(:at_period_end => true)
+      # ci-dessous: méthode pour annuler de l'abonnement automatiquement après le 3e paiement
+      # stripe_subscription.delete(:at_period_end => true)
       # user.subscribed? == false # TODO: trouver un moyen de passer en false un mois plus tard
       ::PaymentMailer.subscription_cancelled(subscription).deliver
     else
